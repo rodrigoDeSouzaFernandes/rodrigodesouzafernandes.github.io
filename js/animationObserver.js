029930d873872fe23ced98472f09452522971ff4
Rodrigo <rdesouza2008@gmaill.com>
2025-06-20 17:33:10 -0300

    apaga comentarios e códigos nao utilizados

--- js/animationObserver.js
+++ js/animationObserver.js
@@ -1,16 +1,19 @@
 const elements = document.querySelectorAll(".animate");
 
-const observer = new IntersectionObserver((entries, observer) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-    //   observer.unobserve(entry.target);
-    }
-  });
-},{
-  threshold: .5, // 50% do elemento precisa estar visível
-  rootMargin: '0px 0px -100px 0px' // ou ajusta conforme a necessidade
-});
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+    rootMargin: "0px 0px -100px 0px",
+  }
+);
 
 elements.forEach((elem) => {
   observer.observe(elem);
