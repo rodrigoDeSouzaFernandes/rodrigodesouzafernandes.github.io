718a2aade12797a29282e75d7f8d869e374ee2f6
rodrigoDeSouzaFernandes <rdesouza2008@gmail.com>
2025-06-24 12:38:07 -0300

    Cria seção de projetos

---
+++ js/visibilityObserver.js
@@ -0,0 +1,34 @@
+const hero = document.querySelector("#hero");
+const header = document.querySelector('header')
+
+const projects = document.querySelectorAll(".project-card");
+
+const headerVisibilityObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      header.classList.remove("visible")
+    } else {
+      header.classList.add("visible")
+    }
+  })
+})
+
+const visibilityObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible")
+    } else {
+      entry.target.classList.remove("visible")
+    }
+  })
+}, {
+  threshold: .7,
+})
+
+headerVisibilityObserver.observe(hero)
+
+console.log(projects)
+
+projects.forEach(project => {
+  visibilityObserver.observe(project)
+})
